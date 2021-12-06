99d6f9af82b1b6c9bc07f4c569e5b00bd6e1ca0c
james <judilsteve>
2021-12-06 18:27:33 +0800

    We have a theme.

--- vsfh-client/src/App.tsx
+++ vsfh-client/src/App.tsx
@@ -1,32 +1,35 @@
 import { BrowserRouter, Routes, Navigate, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
 import SuspensefulComponent from './Routing/SuspensefulComponent';
 
 export const routes = {
-  login: '/Login',
-  acceptInvite: '/AcceptInvite',
-  changePassword: '/ChangePassword',
-  manageUsers: '/Admin/ManageUsers',
-  browseFiles: '/Browse',
-  serverError: '/Error/ServerError',
-  unauthorised: '/Error/Unauthorised',
-  notFound: '/Error/NotFound'
+    login: '/Login',
+    acceptInvite: '/AcceptInvite',
+    changePassword: '/ChangePassword',
+    manageUsers: '/Admin/ManageUsers',
+    browseFiles: '/Browse',
+    serverError: '/Error/ServerError',
+    unauthorised: '/Error/Unauthorised',
+    notFound: '/Error/NotFound'
 };
 
 function App() {
-  return <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Navigate to={routes.browseFiles}/>}/>
-      <Route path={routes.login} element={<SuspensefulComponent importFunc={() => import('./Routes/Login')}/>}/>
-      <Route path={routes.acceptInvite} element={<SuspensefulComponent importFunc={() => import('./Routes/AcceptInvite')}/>}/>
-      <Route path={routes.changePassword} element={<SuspensefulComponent importFunc={() => import('./Routes/ChangePassword')}/>}/>
-      <Route path={routes.manageUsers} element={<SuspensefulComponent importFunc={() => import('./Routes/Admin/ManageUsers')}/>}/>
-      <Route path={routes.browseFiles} element={<SuspensefulComponent importFunc={() => import('./Routes/Browse')}/>}/>
-      <Route path={routes.serverError} element={<SuspensefulComponent importFunc={() => import('./Routes/Error/ServerError')}/>}/>
-      <Route path={routes.unauthorised} element={<SuspensefulComponent importFunc={() => import('./Routes/Error/Unauthorised')}/>}/>
-      <Route path={routes.notFound} element={<SuspensefulComponent importFunc={() => import('./Routes/Error/NotFound')}/>}/>
-      <Route path="*" element={<Navigate to={routes.notFound}/>}/>
-    </Routes>
-  </BrowserRouter>;
+    return <Container>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Navigate to={routes.browseFiles}/>}/>
+                <Route path={routes.login} element={<SuspensefulComponent importFunc={() => import('./Routes/Login')}/>}/>
+                <Route path={routes.acceptInvite} element={<SuspensefulComponent importFunc={() => import('./Routes/AcceptInvite')}/>}/>
+                <Route path={routes.changePassword} element={<SuspensefulComponent importFunc={() => import('./Routes/ChangePassword')}/>}/>
+                <Route path={routes.manageUsers} element={<SuspensefulComponent importFunc={() => import('./Routes/Admin/ManageUsers')}/>}/>
+                <Route path={routes.browseFiles} element={<SuspensefulComponent importFunc={() => import('./Routes/Browse')}/>}/>
+                <Route path={routes.serverError} element={<SuspensefulComponent importFunc={() => import('./Routes/Error/ServerError')}/>}/>
+                <Route path={routes.unauthorised} element={<SuspensefulComponent importFunc={() => import('./Routes/Error/Unauthorised')}/>}/>
+                <Route path={routes.notFound} element={<SuspensefulComponent importFunc={() => import('./Routes/Error/NotFound')}/>}/>
+                <Route path="*" element={<Navigate to={routes.notFound}/>}/>
+            </Routes>
+        </BrowserRouter>
+    </Container>;
 }
 
 export default App;
