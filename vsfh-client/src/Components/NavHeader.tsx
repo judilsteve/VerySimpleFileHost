ac4e6da3f6d082ad929d4f469fb8e0552463e361
james <judilsteve>
2022-01-11 18:30:26 +0800

    Fix bug.

--- vsfh-client/src/Components/NavHeader.tsx
+++ vsfh-client/src/Components/NavHeader.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from "react";
+import { useState, useMemo, useCallback } from "react";
 import { useLocation } from "react-router";
 import { useNavigate } from "react-router";
 import { Link } from "react-router-dom";
@@ -52,7 +52,7 @@ function NavHeader(props: NavHeaderProps) {
 
     const [loggingOut, setLoggingOut] = useState(false);
 
-    const [authStatus, , ] = useEndpointData(getAuthStatus, async e => printResponseError(e, 'auth status'));
+    const [authStatus, , ] = useEndpointData(getAuthStatus, useCallback(async e => printResponseError(e, 'auth status'), []));
     const { isAdministrator } = authStatus ?? { isAdministrator: false };
 
     const [loading, setLoading] = useState(false);
