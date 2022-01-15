3664fa1d87ca65e53373f0ab7a09b5488ed3984e
james <judilsteve>
2022-01-15 13:44:30 +0800

    Clarify testing requirements.

--- vsfh-server/Middleware/RateLimitAnonymousApiRequestsFilter.cs
+++ vsfh-server/Middleware/RateLimitAnonymousApiRequestsFilter.cs
@@ -16,7 +16,13 @@ public class RateLimitAnonymousApiRequestsFilter : IAsyncActionFilter
 
     public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
     {
-        // TODO_JU Test this
+        // TODO_JU Test this:
+        //  - Fill up the semaphore and then attempt to make another unauthenticated API request
+        //      Should spin until a slot becomes available
+        //  - Fill up the semaphore and then attempt to load the login page without an auth cookie
+        //      Page should still load (this filter shouldn't affect static content, only API requests)
+        //  - Fill up the semaphore and then attempt to make an *authenticated* API request
+        //      Should still load
         if(context.HttpContext.User.Identity?.IsAuthenticated ?? false)
         {
             await next();
