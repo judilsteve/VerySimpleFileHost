98517512bd846c08dfd3bfac05565e8a230e6cb5
james <judilsteve>
2022-05-22 12:51:43 +0800

    Prune icon fonts.

--- vsfh-client/preact.config.js
+++ vsfh-client/preact.config.js
@@ -1,10 +1,8 @@
-// TODO_JU Configure pre-rendering (https://preactjs.com/cli/pre-rendering), audit output
-// TODO_JU Investigate https://github.com/pluscubed/preact-cli-plugin-fast-async
-// TODO_JU Investigate https://www.npmjs.com/package/fontmin-webpack
+// TODO_JU Audit pre-rendering output
 // TODO_JU Do I need the ssr-build folder? I want to get rid of it. Same with push-manifest.json and preact_prerender_data.json
 
 const path = require('path');
-const FontminPlugin = require('fontmin-webpack')
+const FontminPlugin = require('fontmin-webpack');
 
 export default (config, env, helpers) => {
   const { rule } = helpers.getLoadersByName(config, 'babel-loader')[0];
@@ -14,12 +12,40 @@ export default (config, env, helpers) => {
   babelConfig.plugins.push(require.resolve("@babel/plugin-proposal-nullish-coalescing-operator"));
   babelConfig.plugins.push(require.resolve("@babel/plugin-proposal-optional-chaining"));
 
-  // Insert before MiniCssExtractPlugin as it causes issues,
-  // see https://github.com/patrickhulce/fontmin-webpack/issues/53
-  // TODO_JU This breaks the build
-  config.plugins.splice(helpers.getPluginsByName(config, 'MiniCssExtractPlugin')[0].index, 0, new FontminPlugin({
-    autodetect: true,
-    allowedFilesRegex: new RegExp('brand-icons*') // TODO_JU This name is probably already mangled at this point
+  //throw new Error(util.inspect(config.plugins.slice(0, 5)));
+
+  // TODO_JU Patch this plugin somehow: https://github.com/patrickhulce/fontmin-webpack/pull/62/commits/005314cbd5da8b3e2a4442cd54a6206ee2b1301e
+  // TODO_JU Audit icons to make sure they still show
+  config.plugins.push(new FontminPlugin({
+    autodetect: false, // Does not work with this build pipeline
+    glyphs: [ // https://fontawesome.com/v5/cheatsheet/free/brands
+      '\uf09b', // github
+    ],
+    allowedFilesRegex: new RegExp('brand-icons')
+  }));
+
+  config.plugins.push(new FontminPlugin({
+    autodetect: false, // Does not work with this build pipeline
+    glyphs: [ // https://fontawesome.com/v5/cheatsheet/free/solid
+      '\uf07c', // folder-open
+      '\uf0c0', // users
+      '\uf084', // key
+      '\uf2f6', // sign-in-alt
+      '\uf101', // angle-double-right
+      '\uf07b', // folder
+      '\uf019', // download
+      '\uf0c1', // linkify
+      '\uf15b', // file
+      '\uf00d', // remove + close
+      '\uf00c', // check
+      '\uf007', // user
+      '\uf09c', // unlock
+      '\uf235', // remove-user
+      '\uf023', // lock
+      '\uf234', // add-user
+      '\uf015', // home
+    ],
+    allowedFilesRegex: new RegExp('icons')
   }));
 
   config.resolve.alias['../../theme.config$'] = path.join(config.context, 'semantic-ui/theme.config');
