49d1fd171d64ee149064117ef06bc1f1f2f638a7
james <judilsteve>
2022-06-26 16:24:32 +0800

    Fix unused font imports, fix not importing both weights for lato

--- vsfh-client/src/index.tsx
+++ vsfh-client/src/index.tsx
@@ -1,6 +1,7 @@
 
 import { h } from 'preact';
-import '@fontsource/lato';
+import '@fontsource/lato/latin-400';
+import '@fontsource/lato/latin-700';
 // TODO_JU Upgrade to fomantic, use some of its new features (e.g. coloured loading spinner, dynamic less compilation)
 import 'semantic-ui-less/definitions/globals/reset.less';
 import 'semantic-ui-less/definitions/globals/site.less';
