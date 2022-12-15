6642cfafbcb4d03af7e36a0759edbb0fd9e6c8cf
james <judilsteve>
2022-12-15 13:08:15 +0800

    Fix frontend build and login bug

--- vsfh-client/src/Utils/sanitiseRedirectUrl.ts
+++ vsfh-client/src/Utils/sanitiseRedirectUrl.ts
@@ -1,5 +1,8 @@
+import safeWindow from "./safeWindow";
+
 export default function sanitiseRedirectUrl(redirect: string) {
+    if(!safeWindow || !redirect) return '';
     // Sanitise to prevent open redirect attack
-    const redirectUrl = new URL(redirect, window.origin);
+    const redirectUrl = new URL(redirect, safeWindow.origin);
     return `${redirectUrl.pathname}${redirectUrl.search}`;
 }
